1b2cc975b3c9d0a94c89cc6997f591f2a1b4d566
Andrija <andrija989@gmail.com>
2019-10-16 14:26:56 +0200

    Add new record - User records page

--- src/Controller/RecordController.php
+++ src/Controller/RecordController.php
@@ -6,10 +6,23 @@ use App\Entity\User;
 use App\Entity\Record;
 Use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RecordController extends AbstractController
 {
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
     /**
      * @Route("/record", name="record")
      */
@@ -24,8 +37,21 @@ class RecordController extends AbstractController
         return $this->render('Users/user.html.twig', ['user' => $user]);
     }
 
-    public function store()
+    public function store(Request $request, $id)
     {
-        
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $record = new Record();
+        $date = new \DateTime();
+        $record->setDate($date);
+        $record->setDistance($request->get('distance'));
+        $record->setTime($request->get('time'));
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+        $record->setUser($user);
+
+        $entityManager->persist($record);
+        $entityManager->flush();
+
+        return new RedirectResponse($this->urlGenerator->generate('home',['id' => $user->getId()]));
     }
 }
